4bf9ee8e67fe129cb331eed7dc5a913f2731a2ce
marciorja <marcio.fibonacci@gmail.com>
2020-06-06 11:42:21 -0300

    Nova versão do mybatis para tratar conexões sem rollback.

--- src/Contexto.js
+++ src/Contexto.js
@@ -7,10 +7,96 @@ function Contexto() {
     this.id = uuid.v4();
 }
 
+function Conexao(connection) {
+    this.connection = connection;
+    this.temTransacao = false;
+}
+
+Conexao.prototype.beginTransaction = function(callback) {
+    this.temTransacao = true;
+
+    this.connection.beginTransaction(callback);
+}
+
+Conexao.prototype.release = function () {
+    if( this.temTransacao ) {
+        console.log('tem que fazer rollback antes');
+        this.connection.rollback( () => {
+            this.connection.release();
+        });
+
+        return;
+    }
+
+    this.connection.release();
+}
+
+Conexao.prototype.query = function(sql, values, cb) {
+    this.connection.query(sql, values, cb);
+}
+
+Conexao.prototype.commit = function(options, callback) {
+    this.temTransacao = false;
+    this.connection.commit(options, callback);
+}
+
+Conexao.prototype.rollback = function(options, callback) {
+    this.temTransacao = false;
+
+    this.connnection.commit(callback);
+}
+
+Conexao.prototype.ping = function(options, callback) {
+    this.connection.ping(options, callback);
+}
+
+Conexao.prototype.end = function end(options, callback) {
+    this.connection.end(options, callback);
+}
+
+Conexao.prototype._handleNetworkError = function(err) {
+    this.connection._handleNetworkError(err);
+};
+
+Conexao.prototype._handleProtocolError = function(err) {
+    this.connection._handleProtocolError(err);
+};
+
+Conexao.prototype._handleProtocolDrain = function() {
+    this.connection._handleProtocolDrain();
+};
+
+Conexao.prototype._handleProtocolConnect = function() {
+    this.connection._handleProtocolConnect();
+};
+
+Conexao.prototype._handleProtocolHandshake = function() {
+    this.connection._handleProtocolHandshake();
+};
+
+Conexao.prototype._handleProtocolInitialize = function(packet) {
+    this.connection._handleProtocolInitialize(packet);
+};
+
+Conexao.prototype._handleProtocolEnd = function(err) {
+    this.connection._handleProtocolEnd(err);
+};
+
+Conexao.prototype._handleProtocolEnqueue = function _handleProtocolEnqueue(sequence) {
+    this.connection._handleProtocolEnqueue(sequence);
+};
+
+Conexao.prototype._implyConnect = function() {
+    if (!this._connectCalled) {
+        this.connect();
+    }
+};
+
 Contexto.prototype = {
 
-    carregou :function(connection){
-        this.conexao = connection ;
+    carregou :function(connection) {
+        this.conexao = new Conexao(connection);
+
         this.carregando = false;
         for(var i=0; i< this.callbacks.length; i++) {
             this.callbacks[i](this.conexao);
@@ -93,7 +179,6 @@ Contexto.prototype = {
 
         });
     }
-
 }
 
 function domainMiddleware(req, res, next) {
